693b08fdcd18a26d4411b7dd86094072503311bc
eduard.balamatiuc <eduard.balamatiuc@smartdata.solutions>
2023-09-11 12:07:14 +0300

    updated the folder structure and implemented the second challenge

---
+++ first_lab/challenge_2.py
@@ -0,0 +1,21 @@
+class MathuUtils:
+    def __init__(self, number):
+        self.number = number
+
+    def extract_multiples_of_3(self):
+        return [i for i in range(1, self.number) if i % 3 == 0]
+    
+    def extract_multiples_of_5(self):
+        return [i for i in range(1, self.number) if i % 5 == 0]
+    
+    def compute_sum_of_multiples_of_3_and_5(self):
+        return sum(self.extract_multiples_of_3()) + sum(self.extract_multiples_of_5())
+    
+    def inform_the_sum(self):
+        sum_of_multiples = self.compute_sum_of_multiples_of_3_and_5()
+        print(sum_of_multiples)
+
+if __name__ == "__main__":
+    number = int(input("Enter a number: "))
+    mathu_utils = MathuUtils(number)
+    mathu_utils.inform_the_sum()
