246fa840105f9dc7d7a17b27da37fc5a7472dda3
eduard.balamatiuc <eduard.balamatiuc@smartdata.solutions>
2023-09-11 12:53:44 +0300

    Created the challenge 6

---
+++ first_lab/challenges/challenge_6.py
@@ -0,0 +1,17 @@
+class MathUtils:
+    def __init__(self, number):
+        self.number = number
+
+    def check_narcisistic_number(self):
+        number = self.number
+        sum_of_digits = 0
+        while number > 0:
+            digit = number % 10
+            sum_of_digits += digit ** 3
+            number //= 10
+        return sum_of_digits == self.number
+    
+if __name__=="__main__":
+    number = int(input("Enter a number: "))
+    math_utils = MathUtils(number)
+    print(math_utils.check_narcisistic_number())
