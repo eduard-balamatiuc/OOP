88f6e3620e3ea3ebc9b00b6324ba5a2ce97e830a
eduard.balamatiuc <eduard.balamatiuc@smartdata.solutions>
2023-10-03 16:36:52 +0300

    add faculty class in separate file

---
+++ second_lab/faculty.py
@@ -0,0 +1,6 @@
+class faculty:
+    def __init__(self, name, abbreviation, students, study_field) -> None:
+        self.name = name
+        self.abbreviation = abbreviation
+        self.students = students
+        self.study_field = study_field
