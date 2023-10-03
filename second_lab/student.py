050f388b2e7f3bac478f7cefad694cb26f67cbb0
eduard.balamatiuc <eduard.balamatiuc@smartdata.solutions>
2023-10-04 01:04:24 +0300

    create attribute change method

--- second_lab/student.py
+++ second_lab/student.py
@@ -1,7 +1,13 @@
-class student:
-    def __init__(self, first_name, last_name, email, enrollment_date, birth_date) -> None:
+class Student:
+    def __init__(self, first_name, last_name, email, enrollment_date, birth_date):
         self.first_name = first_name
         self.last_name = last_name
         self.email = email
         self.enrollment_date = enrollment_date
         self.birth_date = birth_date
+
+    def change_attribute(self, attribute_name, new_value):
+        if hasattr(self, attribute_name):
+            setattr(self, attribute_name, new_value)
+        else:
+            raise AttributeError(f"'{type(self).__name__}' object has no attribute '{attribute_name}'")
