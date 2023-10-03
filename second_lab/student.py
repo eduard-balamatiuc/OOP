c18eee1052a96ff4f42e6850d838f595d9db5063
eduard.balamatiuc <eduard.balamatiuc@smartdata.solutions>
2023-10-04 01:04:44 +0300

    add google style docstrings

--- second_lab/student.py
+++ second_lab/student.py
@@ -1,5 +1,24 @@
 class Student:
+    """A class representing a student.
+
+    Attributes:
+        first_name (str): The first name of the student.
+        last_name (str): The last name of the student.
+        email (str): The email address of the student.
+        enrollment_date (str): The date when the student was enrolled.
+        birth_date (str): The date of birth of the student.
+    """
+
     def __init__(self, first_name, last_name, email, enrollment_date, birth_date):
+        """Initialize a Student object.
+
+        Args:
+            first_name (str): The first name of the student.
+            last_name (str): The last name of the student.
+            email (str): The email address of the student.
+            enrollment_date (str): The date when the student was enrolled.
+            birth_date (str): The date of birth of the student.
+        """
         self.first_name = first_name
         self.last_name = last_name
         self.email = email
@@ -7,6 +26,15 @@ class Student:
         self.birth_date = birth_date
 
     def change_attribute(self, attribute_name, new_value):
+        """Change a student's attribute to a new value.
+
+        Args:
+            attribute_name (str): The name of the attribute to be changed.
+            new_value: The new value to set for the attribute.
+
+        Raises:
+            AttributeError: If the attribute does not exist.
+        """
         if hasattr(self, attribute_name):
             setattr(self, attribute_name, new_value)
         else:
