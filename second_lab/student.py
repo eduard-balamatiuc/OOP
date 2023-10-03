fb15ee985ad6660e0f3fd06cbe36e4f6278e1d15
eduard.balamatiuc <eduard.balamatiuc@smartdata.solutions>
2023-10-03 16:37:04 +0300

    add student class in separate file

---
+++ second_lab/student.py
@@ -0,0 +1,7 @@
+class student:
+    def __init__(self, first_name, last_name, email, enrollment_date, birth_date) -> None:
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = email
+        self.enrollment_date = enrollment_date
+        self.birth_date = birth_date
