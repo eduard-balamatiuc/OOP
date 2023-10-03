262fb8560d44d50b3410a89a663ad58e25e4aba0
eduard.balamatiuc <eduard.balamatiuc@smartdata.solutions>
2023-10-03 16:36:29 +0300

    add main system file with lab_2 folder

---
+++ second_lab/main.py
@@ -0,0 +1,6 @@
+def main():
+    """This is the main function of the TUM student management system"""
+    print("Welcome to the TUM student management system")
+
+if __name__ == "__main__":
+    main()
