580cc6e96b6bf9ba7a080a8e2494be4ea8e45092
pyramixofficial <balamatiuc2@gmail.com>
2023-10-04 23:13:49 +0300

    create tum_structure_class

---
+++ second_lab/tum_structure.py
@@ -0,0 +1,20 @@
+import json
+
+
+class Tum_structure:
+    def __init__(self, structure_location) -> None:
+        self.main_structure = {}
+        self.structure_location = structure_location
+
+    def get_structure(self):
+        return self.main_structure
+    
+    def set_structure(self, structure):
+        self.main_structure = structure
+
+    def get_structure_location(self):
+        return self.structure_location
+    
+    def read_structure(self):
+        with open(self.structure_location, "r") as file:
+            self.main_structure = json.load(file)
