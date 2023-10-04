7d8b779d12dbbf222b68c96d9d8f484f94735fcb
pyramixofficial <balamatiuc2@gmail.com>
2023-10-04 23:09:45 +0300

    add check of available syste versions

--- second_lab/file_management.py
+++ second_lab/file_management.py
@@ -1,6 +1,16 @@
+import os
+
+
 class File_management_system:
     def __init__(self) -> None:
         pass
 
-    def get_available_versions(self, show = False):
-        pass
+    def get_available_files(self):
+        # check if athere is a memory folder and get all the namings all the json files without the extension
+        files = []
+        if os.path.isdir("memory"):
+            files = os.listdir("memory")
+            files = [file.split(".")[0] for file in files]
+        else:
+            os.mkdir("memory")
+        return files
