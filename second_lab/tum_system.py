bf0005b55db12a514e321d18c1b8a8a4b7dcd690
pyramixofficial <balamatiuc2@gmail.com>
2023-10-04 21:54:32 +0300

    add initialization of tum system

--- second_lab/tum_system.py
+++ second_lab/tum_system.py
@@ -1,3 +1,48 @@
-class tum_system:
+from university import University
+from file_management import File_management_system
+
+class Tum_system:
     def __init__(self):
-        pass
+        self.file_management_system = File_management_system
+
+    def initialize_tum_system(self):
+
+        print(
+            """
+            Initializing TUM System...
+            
+            Select and type in the number of the option from below: 
+            0. Use previous system
+            1. Create a new system
+            """
+        )
+
+        initialization_option = input()
+
+        if initialization_option == "0":
+            print(
+                """
+                Below are provided the available systems.
+
+                Please pick one that you would like to continue with  
+                """
+            )
+            self.file_management_system.get_available_versions(show = True)
+            print(
+                """
+                Please type in the number of the system in the list that you would like to continue with
+                """
+            )
+            selected_version = input()
+
+            self.tum = University()
+            
+            self.tum.load_university(input = selected_version)
+            
+        elif initialization_option == "1":
+            self.tum = University()
+        else:
+            print("Invalid option, please try again and use one of the provided options in the description")
+
+            self.initialize_tum_system()
+        
