bf794572fefe5c91d556d49ee5fd62b434eb041f
pyramixofficial <balamatiuc2@gmail.com>
2023-10-05 17:36:02 +0300

    add docstrings to faculty class

--- second_lab/faculty.py
+++ second_lab/faculty.py
@@ -1,16 +1,51 @@
 class Faculty:
-    def __init__(self, name, abbreviation, students, study_field) -> None:
+    """Represents a faculty within a university.
+
+    Attributes:
+        name (str): The name of the faculty.
+        abbreviation (str): The abbreviation or short name of the faculty.
+        students (list): A list of students enrolled in the faculty.
+        study_field (str): The field of study or specialization of the faculty.
+    """
+
+    def __init__(self, name, abbreviation, students, study_field):
+        """Initializes a new Faculty instance.
+
+        Args:
+            name (str): The name of the faculty.
+            abbreviation (str): The abbreviation or short name of the faculty.
+            students (list): A list of students initially enrolled in the faculty.
+            study_field (str): The field of study or specialization of the faculty.
+        """
         self.name = name
         self.abbreviation = abbreviation
         self.students = students
         self.study_field = study_field
-        
+
     def enroll_student(self, student):
+        """Enrolls a student in the faculty.
+
+        Args:
+            student: The student to be enrolled.
+        """
         self.students.append(student)
 
     def graduate_student(self, student):
+        """Graduates a student from the faculty.
+
+        Args:
+            student: The student to be graduated.
+        """
         if student in self.students:
             self.students.remove(student)
 
     def has_student(self, student):
+        """Checks if the faculty has a particular student.
+
+        Args:
+            student: The student to check for.
+
+        Returns:
+            bool: True if the student is enrolled in the faculty, False otherwise.
+        """
         return student in self.students
