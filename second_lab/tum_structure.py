acdb24471db764cce438f860f107d4f7192f5c90
pyramixofficial <balamatiuc2@gmail.com>
2023-10-08 17:33:41 +0300

    add default value for selected_file in tum_structure class

--- second_lab/tum_structure.py
+++ second_lab/tum_structure.py
@@ -5,7 +5,7 @@ from file_management import File_management_system as fms
 class Tum_structure:
     def __init__(
         self,
-        selected_file,
+        selected_file: str = None,
     ) -> None:
         self.selected_file = selected_file
         self.university = University(fms.get_structure_file(self.selected_file))
