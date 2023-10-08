52e766024ceb6b88cd5243b8403642f746317aec
pyramixofficial <balamatiuc2@gmail.com>
2023-10-08 17:34:04 +0300

    add separated university intialization process

--- second_lab/tum_structure.py
+++ second_lab/tum_structure.py
@@ -1,11 +1,16 @@
 from university import University
 from file_management import File_management_system as fms
 
-
 class Tum_structure:
     def __init__(
         self,
         selected_file: str = None,
     ) -> None:
         self.selected_file = selected_file
-        self.university = University(fms.get_structure_file(self.selected_file))
+        self.university = self.initialize_university()
+
+    def initialize_university(self):
+        if self.selected_file:
+            return University(fms.get_structure_file(self.selected_file))
+        else:
+            return University()
