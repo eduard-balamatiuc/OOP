c5184bf7f38590027c06cf57d457ac0331b2c18a
pyramixofficial <balamatiuc2@gmail.com>
2023-10-09 03:31:57 +0300

    update docstrings in tum structure

--- second_lab/tum_structure.py
+++ second_lab/tum_structure.py
@@ -2,14 +2,19 @@ from university import University
 from file_management import File_management_system as fms
 
 class Tum_structure:
+    """A class representing the TUM structure."""
     def __init__(
         self,
         selected_file: str = None,
     ) -> None:
+        """Initializes a new instance of the Tum_structure class."""
         self.selected_file = selected_file
         self.university = self.initialize_university()
 
-    def initialize_university(self):
+    def initialize_university(
+        self,
+    ):
+        """Initialize the university object."""
         if self.selected_file:
             return University(fms.get_structure_file(self.selected_file))
         else:
