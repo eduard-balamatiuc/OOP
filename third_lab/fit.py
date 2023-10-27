cc61fdcd3111b8720296fd550fcb37ebd3710b61
pyramixofficial <balamatiuc2@gmail.com>
2023-10-27 17:44:27 +0300

    add snapshot function

--- third_lab/fit.py
+++ third_lab/fit.py
@@ -1,5 +1,6 @@
 import os
 import json
+from file import File
 
 class Fit:
     """A class structure that will mimic the git functionalities of git"""
@@ -34,25 +35,39 @@ class Fit:
         # create the commit history json file in the hidden folder
         file_name = "fit_info.json"
         file_path = os.path.join(self.__fit_folder_path, file_name)
+        self.__fit_info["tracked"] = {}
+        self.__fit_info["staged"] = {}
+        self.__fit_info["commits"] = {}
+        self.__fit_info["untracked"] = {}
         
         try:
-            open(file_path, 'w').close()
+            with open(file_path, "w") as json_file:
+                json.dump(self.__fit_info, json_file)
         except OSError:
             print("Creation of the file %s failed" % file_path)
             return False
         
         return True
     
-    def fit_get_status():
-        """This is a method that will get the status of the system."""
-        
+
     
-    def fit_add_file(request_parameters):
-        pass
-    
-    def fit_commit_changes(request_parameters):
-        pass
-    
-    def fit_info_about_files(request_parameters):
-        pass
-    
+    def take_snapshot(self):
+        """This is a method that will take a snapshot of the current folder and return a dict with the files."""
+        snapshot = {}
+        fit_directory = os.path.join(os.getcwd(), ".fit")
+        py_cache = os.path.join(os.getcwd(), "__pycache__")
+        for root, dirs, files in os.walk(os.getcwd()):
+            if root == fit_directory or root == py_cache:
+                continue
+            for file_name in files:
+                file_path = os.path.join(root, file_name)
+                snapshot[file_name] = File(
+                    path=file_path,
+                    name=file_name.split(".")[0],
+                    extension=file_name.split(".")[-1],
+                    size=os.path.getsize(file_path),
+                    created_at=os.path.getctime(file_path),
+                    updated_at=os.path.getmtime(file_path),
+                    properties={},
+                )
+        return snapshot
