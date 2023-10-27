4fa72e64bd6a129059be5f9809f550ff51f0480d
pyramixofficial <balamatiuc2@gmail.com>
2023-10-27 17:44:03 +0300

    added Fit class as a private method fo the interaction

--- third_lab/fit_interaction.py
+++ third_lab/fit_interaction.py
@@ -7,6 +7,7 @@ class FitInteraction:
         self.__state = "init"
         self.__request = ""
         self.__request_parameters = []
+        self.__Fit = Fit()
     
     def interaction_initialization(self):
         """This is a method that will start the system and initialize it."""
@@ -65,45 +66,45 @@ class FitInteraction:
         
     def fit_init(self):
         """This is a method that will initialize a fit system in the current folder."""
-        if Fit.fit_check_hidden_system_folder():
+        if self.__Fit.fit_check_hidden_system_folder():
             print("The system is already initialized!")
-        elif Fit.fit_create_hidden_system():
+        elif self.__Fit.fit_create_hidden_system():
             print("The system was initialized successfully!")
         else:
             print("The system could not be initialized!")
             
     def fit_status(self):
         """This is a method that will check the status of the system."""
-        if not Fit.fit_check_hidden_system_folder():
+        if not self.__Fit.fit_check_hidden_system_folder():
             print("The system is not initialized!")
             return
         
-        Fit.fit_get_status()
+        self.__Fit.fit_get_status()
         
     def fit_add(self):
         """This is a method that will add a file to the system."""
-        if not Fit.fit_check_hidden_system_folder():
+        if not self.__Fit.fit_check_hidden_system_folder():
             print("The system is not initialized!")
             return
         
-        if Fit.fit_add_file(self.__request_parameters):
+        if self.__Fit.fit_add_file(self.__request_parameters):
             print("The files were added successfully!")
             
     def fit_commit(self):
         """This is a method that will commit the changes to the system."""
-        if not Fit.fit_check_hidden_system_folder():
+        if not self.__Fit.fit_check_hidden_system_folder():
             print("The system is not initialized!")
             return
         
-        if Fit.fit_commit_changes(self.__request_parameters):
+        if self.__Fit.fit_commit_changes(self.__request_parameters):
             print("The changes were committed successfully!")
             
     def fit_info(self):
         """This is a method that will print the info about the files"""
-        if not Fit.fit_check_hidden_system_folder():
+        if not self.__Fit.fit_check_hidden_system_folder():
             print("The system is not initialized!")
             return
         
-        if Fit.fit_info_about_files(self.__request_parameters):
+        if self.__Fit.fit_info_about_files(self.__request_parameters):
             print("The info was printed successfully!")
     
