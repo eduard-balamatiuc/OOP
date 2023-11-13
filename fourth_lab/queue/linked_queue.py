747e4cbc35c582ca934f2675affa3d7c9a188ab2
eduard.balamatiuc <eduard.balamatiuc@smartdata.solutions>
2023-11-13 22:06:42 +0200

    add enqueue method for linked

--- fourth_lab/queue/linked_queue.py
+++ fourth_lab/queue/linked_queue.py
@@ -24,4 +24,19 @@ class LinkedQueue:
         self.rear = None
         self.size = 0
 
- 
+    def enqueue(self, element):
+        """
+        Adds an element to the rear of the queue.
+
+        Args:
+            element: The element to be added to the queue.
+        """
+        new_node = self.Node(element)
+        if self.is_empty():
+            self.front = new_node
+            self.rear = new_node
+        else:
+            self.rear.next = new_node
+            self.rear = new_node
+        self.size += 1
+
