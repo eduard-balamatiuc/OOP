5ec99c9e3e8cd818c0a4109a5116dd8eedf3750c
eduard.balamatiuc <eduard.balamatiuc@smartdata.solutions>
2023-11-13 20:53:10 +0200

    create linked stack class with node class inside

--- fourth_lab/stack/linked_stack.py
+++ fourth_lab/stack/linked_stack.py
@@ -2,3 +2,24 @@ class EmptyStackException(Exception):
     """Exception raised when performing an operation on an empty stack."""
     pass
 
+class LinkedStack:
+    """Implements a stack data structure using a linked list."""
+
+    class Node:
+        """A node in the linked list used for the stack."""
+
+        def __init__(self, data):
+            """
+            Initializes a Node with the given data.
+
+            Args:
+                data: The data to be stored in the node.
+            """
+            self.data = data
+            self.next = None
+
+    def __init__(self):
+        """Initializes an empty LinkedStack."""
+        self.head = None
+        self.size = 0
+
