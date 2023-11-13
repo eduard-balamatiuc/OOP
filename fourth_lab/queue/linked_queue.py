26b478dc8ee39357d8e06137e9cd36172d063e8a
eduard.balamatiuc <eduard.balamatiuc@smartdata.solutions>
2023-11-13 22:06:23 +0200

    add linked queue class and class node

--- fourth_lab/queue/linked_queue.py
+++ fourth_lab/queue/linked_queue.py
@@ -2,3 +2,26 @@ class EmptyQueueException(Exception):
     """Exception raised when performing an operation on an empty queue."""
     pass
 
+class LinkedQueue:
+    """Implements a queue data structure using a linked list."""
+
+    class Node:
+        """A node in the linked list used for the queue."""
+
+        def __init__(self, data):
+            """
+            Initializes a Node with the given data.
+
+            Args:
+                data: The data to be stored in the node.
+            """
+            self.data = data
+            self.next = None
+
+    def __init__(self):
+        """Initializes an empty queue."""
+        self.front = None
+        self.rear = None
+        self.size = 0
+
+ 
