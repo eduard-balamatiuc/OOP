9c960700f41368a55b81baa34625ed7621942ed0
pyramixofficial <balamatiuc2@gmail.com>
2023-12-07 14:07:15 +0200

    implement linked test

--- fourth_lab/queue/test_linked.py
+++ fourth_lab/queue/test_linked.py
@@ -1,35 +1,77 @@
-from linked_queue import LinkedQueue
+from linked_queue import LinkedListQueue
+from abstract_queue import EmptyQueueException
 
-queue = LinkedQueue()
+import unittest
 
-print("Enqueueing elements 10, 20, and 30")
-queue.enqueue(10)
-queue.enqueue(20)
-queue.enqueue(30)
+class TestLinkedListQueue(unittest.TestCase):
+    """
+    A class that contains unit tests for the LinkedListQueue class.
 
-# Display front element
-print("Front element:", queue.peek())
+    Methods:
+    - test_enqueue: Test the enqueue method of LinkedListQueue.
+    - test_dequeue: Test the dequeue method of LinkedListQueue.
+    - test_peek: Test the peek method of LinkedListQueue.
+    - test_is_empty: Test the is_empty method of LinkedListQueue.
+    - test_get_size: Test the get_size method of LinkedListQueue.
+    - test_empty_dequeue: Test the behavior of dequeue when the queue is empty.
+    - test_empty_peek: Test the behavior of peek when the queue is empty.
+    """
+    
+    def test_enqueue(self):
+        """Test the enqueue method of LinkedListQueue."""
+        queue = LinkedListQueue()
+        queue.enqueue(1)
+        self.assertEqual(queue.peek(), 1)
+        self.assertEqual(queue.get_size(), 1)
 
-# Dequeue two elements
-print("Dequeued:", queue.dequeue())
-print("Dequeued:", queue.dequeue())
+    def test_dequeue(self):
+        """Test the dequeue method of LinkedListQueue."""
+        queue = LinkedListQueue()
+        queue.enqueue(1)
+        queue.enqueue(2)
+        value = queue.dequeue()
+        self.assertEqual(value, 1)
+        self.assertEqual(queue.get_size(), 1)
+        self.assertEqual(queue.peek(), 2)
 
-# Display current front element
-print("Current front element:", queue.peek())
+    def test_peek(self):
+        """Test the peek method of LinkedListQueue."""
+        queue = LinkedListQueue()
+        queue.enqueue(1)
+        queue.enqueue(2)
+        self.assertEqual(queue.peek(), 1)
 
-# Enqueue another element
-print("Enqueueing element 40")
-queue.enqueue(40)
+    def test_is_empty(self):
+        """Test the is_empty method of LinkedListQueue."""
+        queue = LinkedListQueue()
+        self.assertTrue(queue.is_empty())
+        queue.enqueue(1)
+        self.assertFalse(queue.is_empty())
+        queue.dequeue()
+        self.assertTrue(queue.is_empty())
 
-# Display the size of the queue
-print("Queue size:", queue.get_size())
+    def test_get_size(self):
+        """Test the get_size method of LinkedListQueue."""
+        queue = LinkedListQueue()
+        self.assertEqual(queue.get_size(), 0)
+        queue.enqueue(1)
+        self.assertEqual(queue.get_size(), 1)
+        queue.enqueue(2)
+        self.assertEqual(queue.get_size(), 2)
+        queue.dequeue()
+        self.assertEqual(queue.get_size(), 1)
 
-# Check if queue is empty
-print("Is queue empty?:", queue.is_empty())
+    def test_empty_dequeue(self):
+        """Test the behavior of dequeue when the queue is empty."""
+        queue = LinkedListQueue()
+        with self.assertRaises(EmptyQueueException):
+            queue.dequeue()
 
-# Clear the queue
-print("Clearing the queue")
-queue.clear()
+    def test_empty_peek(self):
+        """Test the behavior of peek when the queue is empty."""
+        queue = LinkedListQueue()
+        with self.assertRaises(EmptyQueueException):
+            queue.peek()
 
-# Check if queue is empty after clearing
-print("Is queue empty after clearing?:", queue.is_empty())
+if __name__ == '__main__':
+    unittest.main()
