e45247c138842e5e5f941cb3b3657b9c405aea66
pyramixofficial <balamatiuc2@gmail.com>
2023-12-07 12:59:16 +0200

    implement array up test

---
+++ fourth_lab/stack/test_array_up.py
@@ -0,0 +1,63 @@
+from array_up import ArrayStack
+from abstract_stack import EmptyStackException
+
+import unittest
+
+
+class TestArrayStack(unittest.TestCase):
+
+    def test_push(self):
+        stack = ArrayStack()
+        stack.push(1)
+        self.assertEqual(stack.peek(), 1)
+        self.assertEqual(stack.get_size(), 1)
+
+    def test_push_resizing(self):
+        stack = ArrayStack()
+        for i in range(ArrayStack.INITIAL_SIZE):
+            stack.push(i)
+        # At this point, the stack should resize as we add the next element.
+        stack.push(ArrayStack.INITIAL_SIZE)
+        self.assertEqual(stack.peek(), ArrayStack.INITIAL_SIZE)
+        self.assertEqual(stack.get_size(), ArrayStack.INITIAL_SIZE + 1)
+
+    def test_pop(self):
+        stack = ArrayStack()
+        stack.push(1)
+        stack.push(2)
+        value = stack.pop()
+        self.assertEqual(value, 2)
+        self.assertEqual(stack.get_size(), 1)
+
+    def test_peek(self):
+        stack = ArrayStack()
+        stack.push(1)
+        stack.push(2)
+        self.assertEqual(stack.peek(), 2)
+        self.assertEqual(stack.get_size(), 2)
+        stack.pop()
+        self.assertEqual(stack.peek(), 1)
+
+    def test_is_empty(self):
+        stack = ArrayStack()
+        self.assertTrue(stack.is_empty())
+        stack.push(1)
+        self.assertFalse(stack.is_empty())
+
+    def test_get_size(self):
+        stack = ArrayStack()
+        self.assertEqual(stack.get_size(), 0)
+        for i in range(5):
+            stack.push(i)
+        self.assertEqual(stack.get_size(), 5)
+
+    def test_empty_pop(self):
+        stack = ArrayStack()
+        self.assertRaises(EmptyStackException, stack.pop)
+
+    def test_empty_peek(self):
+        stack = ArrayStack()
+        self.assertRaises(EmptyStackException, stack.peek)
+
+if __name__ == '__main__':
+    unittest.main()
